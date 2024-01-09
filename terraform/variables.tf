dd99a840219b0762b0c56b831cc25de7957a1399
lukasfe <lukasferencik@gmail.com>
2024-01-09 13:23:58 +0100

    kubeconfig fixed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,7 @@ variable "kube_config_context" {
   description = "Kubernetes context to use for deployment"
   default     = "portfolio"
 }
+variable "kube_config_path" {
+  description = "Path to the kubeconfig file"
+  default     = "kubeconfig"
+}
